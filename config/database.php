1c71f8154bfd46d07357f00e86b70649aa0fa451
demoncowgirl <bethsalvatore2018@gmail.com>
2020-10-27 23:18:43 -0500

    updates to package.json and database config

--- config/database.php
+++ config/database.php
@@ -39,13 +39,72 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+        // 'sqlite' => [
+        //     'driver' => 'sqlite',
+        //     'database' => env('DB_DATABASE', database_path('database.sqlite')),
+        //     'prefix' => '',
+        // ],
+
+        'mysql' => [
+          'read' => [
+            'host' => [
+              $host,
+            ],
+          ],
+          'write' => [
+            'host' => [
+              $host,
+            ],
+          ],
+            'sticky' => true,
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'mysql1' => [
+          'read' => [
+            'host' => [
+              'localhost',
+            ],
+          ],
+          'write' => [
+            'host' => [
+              'localhost',
+            ],
+          ],
+          ' sticky' => true,
+          'driver' => 'mysql',
+          'host' => env('DB_HOST', 'localhost'),
+          'port' => env('DB_PORT', '3306'),
+          'database' => env('DB_DATABASE', 'cards'),
+          'username' => env('DB_USERNAME', 'root'),
+          'password' => env('DB_PASSWORD', ''),
+          'unix_socket' => env('DB_SOCKET', ''),
+          'charset' => 'utf8mb4',
+          'collation' => 'utf8mb4_unicode_ci',
+          'prefix' => '',
+          'strict' => true,
+          'engine' => null,
+      ],
+
+      // 'sqlsrv' => [
+      //      'driver' => 'sqlsrv',
+      //      'host' => env('DB_HOST', 'localhost'),
+      //      'port' => env('DB_PORT', '1433'),
+      //      'database' => env('DB_DATABASE', 'forge'),
+      //      'username' => env('DB_USERNAME', 'forge'),
+      //      'password' => env('DB_PASSWORD', ''),
+      //      'charset' => 'utf8',
+      //      'prefix' => '',
+      //  ],
+
             'driver' => 'mysql',
             'host' => env('CLEAR_DB_HOST', ''),
             'database' => env('CLEAR_DB_DATABASE', ''),
@@ -71,19 +130,17 @@ return [
         //     'sslmode' => 'prefer',
         // ],
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
-
-    ],
+        // 'sqlsrv' => [
+        //     'driver' => 'sqlsrv',
+        //     'host' => env('DB_HOST', 'localhost'),
+        //     'port' => env('DB_PORT', '1433'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        //     'username' => env('DB_USERNAME', 'forge'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'charset' => 'utf8',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        // ],
 
     /*
     |--------------------------------------------------------------------------
